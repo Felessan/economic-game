dd6d7e76f1c03403627462ba8aca68e16b3ad547
Tay_Veter <tay.listay@gmail.com>
2018-01-01 16:38:08 +0300

    Комментарии

--- src/main/java/ru/necklace/rebel/economic/model/EconomicSituation.java
+++ src/main/java/ru/necklace/rebel/economic/model/EconomicSituation.java
@@ -41,6 +41,11 @@ public class EconomicSituation {
         return this.commands;
     }
 
+    /**
+     * Загрузка начальной ситуации из файла
+     * @param dataPath - путь до файла с исходными данными
+     * @throws IOException - А вдруг файл не найден?
+     */
     public void load(String dataPath) throws IOException {
         this.items = new ArrayList<>();
         this.commands = new ArrayList<>();
@@ -50,24 +55,33 @@ public class EconomicSituation {
         Iterator rowIterator = sheet1.iterator();
 
         while(rowIterator.hasNext()) {
+            //Перебираем строчки
             Row row = (Row)rowIterator.next();
             if (row.getRowNum() != 0) {
                 if (allItemsRead) {
+                    //Если строка не первая и у нас взведён флаг, что прочитаны все товары, принудительно выходим из цикла
                     break;
                 }
+                //Если строка не первая, но флаг прочитанности всех товаров ещё не взведён, перебираем ячейки в строке
+                ItemInfo addingItem = new ItemInfo(); //заводим новый товар
+                Iterator cellIterator = row.iterator(); //заводим итератор - переборщик - для ячеек в строке
 
-                ItemInfo addingItem = new ItemInfo();
-                Iterator cellIterator = row.iterator();
+                while(cellIterator.hasNext()) {//Пока есть следующие ячейки, работаем
 
-                while(cellIterator.hasNext()) {
                     Cell cell = (Cell)cellIterator.next();
+                    //Берём ячейку
                     if (cell.getColumnIndex() == 0) {
+                        //Если номер в строке - 0, это название товара
                         if (cell.getCellType() != 1) {
+                            //Если тип ячейки не 1, считаем, что ячейка пустая и товары все прочитаны.
                             allItemsRead = true;
                         } else {
+                            //Если тип ячейки всё таки 1, считаем, что это название товара
                             addingItem.setItemName(cell.getRichStringCellValue().getString());
                         }
                     } else if (cell.getColumnIndex() == 1) {
+                        //Если номер в строке - 1, это количество на рынке - пробуем преобразовать в число и
+                        // подставить начальное количество и добычу в первом раунде
                         try {
                             addingItem.setAvailable(Math.round(cell.getNumericCellValue()));
                             addingItem.setIncome(Math.round((double)addingItem.getAvailable() * 0.2D));
@@ -75,23 +89,29 @@ public class EconomicSituation {
                             //logger.error("Неверно указан параметр \"Доступно\" для " + addingItem.getItemName());
                         }
                     } else if (cell.getColumnIndex() == 2) {
+                        //Если номер в строке 2 - это начальная цена. Аналогично пробуем преобразовать в число
                         try {
                             addingItem.setCost(Math.round(cell.getNumericCellValue()));
                         } catch (NumberFormatException var14) {
                             //logger.error("Неверно указан параметр \"Цена\" для " + addingItem.getItemName());
                         }
                     }
+                    //нужно поднять флаг по пустым ячейкам, его нет)) Короче надо учесть
                 }
 
+                //Добавляем свежезагруженный из файла товар в список товаров
                 this.items.add(addingItem);
+                //Устанавливаем товару ручную коррекцию имеющегося количества 0 - костыль!
                 this.itemsIncomeCorrection.put(addingItem.getItemName(), 0);
             }
         }
 
+        //Берём второй лист эксельника
         Sheet sheet2 = workbook.getSheetAt(1);
         boolean allCommandsRead = false;
         Iterator commandRowIterator = sheet2.iterator();
 
+        //На втором листе - команды, их читаем аналогично - итератором построчно
         while(commandRowIterator.hasNext()) {
             Row row = (Row)commandRowIterator.next();
             if (row.getRowNum() != 0) {
@@ -126,17 +146,31 @@ public class EconomicSituation {
             }
         }
 
+        //Не забываем закрыть файл после чтения, чтобы не сломать его
         workbook.close();
     }
 
+    /**
+     * Метод перехода к следующему ходу
+     */
     public void nextStep() {
+        //Увеличиваем номер хода, возможно - опять костыль
+        //TODO перенести в dev
+        //В новой архитектуре костылём не будет
         ++this.step;
+
+        //Костыльным образом делаем слепок текущей рыночной ситуации
         EconomicSituation prevTurnSituation = new EconomicSituation(this.coefficients);
+
+        //Перебираем все товары на рынке
         Iterator i$ = this.items.iterator();
 
         while(i$.hasNext()) {
             ItemInfo item = (ItemInfo)i$.next();
+            //Костыльным образом делаем новый товар, частично дублирующий информацию из старого
             ItemInfo updatedItem = new ItemInfo();
+            //В новом слепке информации о товаре проставляем прирост товара исходя из количества проданного за ход,
+            //указанного в старой информации о товаре
             if ((double)item.getSold() > this.coefficients.getCoefficient(PropertiesName.EXTRA_LARGE_SALES) * (double)item.getBaseAvailable()) {
                 updatedItem.setIncome(Math.round((double)item.getBaseIncome() * (1.0D + this.coefficients.getCoefficient(PropertiesName.EXTRA_LARGE_INPUT))));
             } else if ((double)item.getSold() > this.coefficients.getCoefficient(PropertiesName.LARGE_SALES) * (double)item.getBaseAvailable()) {
@@ -145,28 +179,38 @@ public class EconomicSituation {
                 updatedItem.setIncome(item.getBaseIncome());
             }
 
+            //Добавляем в прирост ручные корректировки, осуществлённые на этом ходу
             updatedItem.setIncome(updatedItem.getIncome() + (long) (Integer) this.itemsIncomeCorrection.get(item.getItemName()));
+            //Трём данные о ручных корректировках
             this.itemsIncomeCorrection.put(item.getItemName(), 0);
+            //Устанавливаем новое значение доступного количества товара
             updatedItem.setAvailable(item.getAvailable() + updatedItem.getIncome() - item.getSold());
+            //Устанавливаем цену на следующем раунде
             updatedItem.setCost(Math.round((double)item.getCost() * (1.0D + this.coefficients.getCoefficient(PropertiesName.COST_ENLARGE) * (double)(item.getBaseAvailable() - updatedItem.getAvailable()) / (double)item.getBaseAvailable())));
+            //В непонятно, зачем нужный слепок ситуации предыдущего хода записываем ЕЩЁ ОДНУ копию информации о товаре
             prevTurnSituation.getMarket().add(item.getCopy());
+            //Переставляем данные из нового хода в текущий
             item.setSold(0L);
             item.setAvailable(updatedItem.getAvailable());
             item.setIncome(updatedItem.getIncome());
             item.setCost(updatedItem.getCost());
         }
 
+        //Товары перебрали, перебираем команды
         i$ = this.commands.iterator();
 
         while(i$.hasNext()) {
             CommandInfo command = (CommandInfo)i$.next();
             CommandInfo oldCommandState = new CommandInfo();
+            //Делаем копию текущего состояния команды
             oldCommandState.setCommandName(command.getCommandName());
             oldCommandState.setCurrentCash(command.getCurrentCash());
             oldCommandState.setExpenditure(command.getExpenditure());
             oldCommandState.setIncome(command.getIncome());
             oldCommandState.setProbableCash(command.getProbableCash());
+            //Сохраняем сделанную копию в старый слепок
             prevTurnSituation.getCommands().add(oldCommandState);
+            //в текущем значении команды устанавливаем новые значения - которые рассчитывались прямо в процессе хода, похоже
             command.setCurrentCash(command.getProbableCash());
             command.setExpenditure(0L);
             command.setIncome(0L);
@@ -182,6 +226,5 @@ public class EconomicSituation {
             this.commands = ((EconomicSituation)this.history.get(this.history.size() - 1)).getCommands();
             this.history.remove(this.history.size() - 1);
         }
-
     }
 }
